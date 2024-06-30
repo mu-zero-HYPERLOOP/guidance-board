f200e3d01e9874aeaecb325179449d8db38c4a31
kistenklaus <karlsasssie@gmail.com>
2024-06-30 02:42:18 +0200

    fuck precharge

--- src/fsm/states/precharge.cpp
+++ src/fsm/states/precharge.cpp
@@ -9,18 +9,20 @@ constexpr Duration MIN_PRECHARGE_TIME = 1_s;
 constexpr Duration MAX_PRECHARGE_TIME = 5_s;
 constexpr Voltage REQUIRED_VDC_VOLTAGE = 40_V;
 
-guidance_state fsm::states::precharge(guidance_command cmd, Duration time_since_last_transition) {
-  
+guidance_state fsm::states::precharge(guidance_command cmd,
+                                      Duration time_since_last_transition) {
+
   if (guidance_command_DISARM45 == cmd) {
     return guidance_state_DISARMING45;
   }
 
-  if (time_since_last_transition > MIN_PRECHARGE_TIME && canzero_get_vdc_voltage() > static_cast<float>(REQUIRED_VDC_VOLTAGE)) {
+  if (time_since_last_transition > MIN_PRECHARGE_TIME){
     return guidance_state_READY;
   }
 
   if (time_since_last_transition > MAX_PRECHARGE_TIME) {
-    return guidance_state_DISARMING45; // we might want to set a error flag here.
+    return guidance_state_DISARMING45; // we might want to set a error flag
+                                       // here.
   }
 
   pwm::control(PwmControl());
@@ -28,7 +30,7 @@ guidance_state fsm::states::precharge(guidance_command cmd, Duration time_since_
   pwm::enable_trig0();
   pwm::enable_trig1();
 
-  if (!sdc_brake::request_close()){
+  if (!sdc_brake::request_close()) {
     canzero_set_command(guidance_command_DISARM45);
     return guidance_state_DISARMING45;
   }
