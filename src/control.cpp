c7c85d16cc92ed6ef5e90d630b85bb6d8a06ecdb
Jakob Schmid <jakobschmid25.12@gmail.com>
2024-07-20 00:12:37 +0200

    adapt PID

--- src/control.cpp
+++ src/control.cpp
@@ -51,9 +51,9 @@ static PidParameters pid_force_parameters;
 void control::begin() {
 
   // Parameters
-  pid_force_parameters.Kp = 2;
+  pid_force_parameters.Kp = 0.1;
   pid_force_parameters.Ki = 0;
-  pid_force_parameters.Kd = 0.1;
+  pid_force_parameters.Kd = 0.5;
 
   pi_left_current_parameters.Kp = 20;
   pi_left_current_parameters.Ki = 10;
